3b3f45b9cc7de15b850b39b4f86bd33f8919f0a3
EdgarTipan <evtipanl1@uce.edu.ec>
2025-05-15 23:26:56 -0500

    Tarea #3

--- src/components/Estudiante.vue
+++ src/components/Estudiante.vue
@@ -1,15 +1,24 @@
 <template>
   <div class="container">
     <label for="id_nombre">Nombre: </label>
-    <input v-model="nuevoNombre" id="id_nombre" type="text" />
+    <input v-model="nuevoNombre" id="id_nombre" type="text" /> <hr>
     <label for="id_apellido">Apellido: </label>
-    <input v-model="nuevoApellido" id="id_apellido" type="text" />
+    <input v-model="nuevoApellido" id="id_apellido" type="text" /> <hr>
+    <label for="id_edad">Edad: </label>
+    <input v-model="nuevaEdad" id="id_edad" type="number" /> <hr>
+    <label for="id_genero">Genero: </label>
+    <input v-model="nuevoGenero" id="id_genero" type="text" /> <hr>
+    <label for="id_semestre">Semestre: </label>
+    <input v-model="nuevoSemestre" id="id_semestre" type="number" /> <hr>
     <button v-on:click="agregarEstudiante()">Agregar estudiante</button>
     {{ nuevoNombre }}
     {{ nuevoApellido }}
+    {{ nuevaEdad }}
+    {{ nuevoGenero }}
+    {{ nuevoSemestre }}
     <ul>
-      <li v-for="{ nombre, apellido } in lista" :key="nombre">
-        Nombre: {{ nombre }} - Apellido: {{ apellido }}
+      <li v-for="{ nombre, apellido, edad, genero, semestre } in lista" :key="nombre">
+        Nombre: {{ nombre }} - Apellido: {{ apellido }} - Edad: {{ edad }} - Genero: {{ genero }} - Semestre: {{ semestre }}
       </li>
     </ul>
   </div>
@@ -21,14 +30,17 @@ export default {
     return {
       nuevoNombre: "Nuevo",
       nuevoApellido: "Estudiante",
+      nuevaEdad: 0,
+      nuevoGenero: "-",
+      nuevoSemestre: 0,
       lista: [
-        { nombre: "Edgar", apellido: "Tipan" },
-        { nombre: "Daniel", apellido: "Tipan" },
-        { nombre: "Pedro", apellido: "Teran" },
-        { nombre: "Andrea", apellido: "Tipan" },
-        { nombre: "Carlos", apellido: "Castillo" },
-        { nombre: "Ana", apellido: "Tobar" },
-        { nombre: "Viviana", apellido: "Lopez" },
+        { nombre: "Edgar", apellido: "Tipan", edad: 18, genero: "Masculino", semestre: 8 },
+        { nombre: "Daniel", apellido: "Tipan", edad: 21, genero: "Masculino", semestre: 7 },
+        { nombre: "Pedro", apellido: "Teran", edad: 23, genero: "Masculino", semestre: 9 },
+        { nombre: "Andrea", apellido: "Tipan", edad: 19, genero: "Femenino", semestre: 6 },
+        { nombre: "Carlos", apellido: "Castillo", edad: 20, genero: "Masculino", semestre: 8 },
+        { nombre: "Ana", apellido: "Tobar", edad: 22, genero: "Femenino", semestre: 7 },
+        { nombre: "Viviana", apellido: "Lopez", edad: 21, genero: "Femenino", semestre: 8 },
       ],
     };
   },
@@ -37,6 +49,9 @@ export default {
       const nuevo = {
         nombre: this.nuevoNombre,
         apellido: this.nuevoApellido,
+        edad: this.nuevaEdad,
+        genero: this.nuevoGenero,
+        semestre: this.nuevoSemestre,
       };
       //this.lista.unshift(nuevo);
       this.lista.push(nuevo);
